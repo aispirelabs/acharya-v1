46bbcccdbd0468b00449d64b8301e6b7946d06fd
aispirelabs <aispirelabs@gmail.com>
2025-06-19 12:51:43 +0530

    Delete components directory

--- components/Logo.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Logo() {
-  return (
-    <Link href="/dashboard" className="text-2xl font-bold gradient-text">
-      AcharyaAI
-    </Link>
-  );
-}
